7247c7de5b8c8fed6137542dad91b861420b3167
Ummgulsum Koc <umgk86@gmail.com>
2020-10-18 23:17:07 +0200

    updated the way we say hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,10 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("sill awesome hello");
+        System.out.println("sill really awesome hello");
+
+
+
     }
 
 
