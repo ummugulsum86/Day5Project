28617817cce47fbaac0b55ef96faa13d727fdc60
Ummgulsum Koc <umgk86@gmail.com>
2020-10-18 23:45:12 +0200

    added comment on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("sill really awesome hello");
 
+        //add one more line on your local and commit
+
 
 
     }
