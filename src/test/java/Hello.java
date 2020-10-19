e5248b6151e1ccbb69d74eb07dd682de0469418a
ummugulsum86 <72078416+ummugulsum86@users.noreply.github.com>
2020-10-19 13:34:47 +0200

    co worker made this change and pushed
    this will conflict

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,12 +3,9 @@ public class Hello {
     public static void main(String[] args) {
 
 
+  System.out.println("sill really my coworker added awesome hello");//we are simulating the situation my cower made this change !!!co-worker made this change and push to 
 
-        System.out.println("sill really awesome hello");//added comment
-=========
-        System.out.println("silly really fmy cower added rom github made some changes awesome hello");//we are simulating this situatuin
- >>>>>>>>>origin/master
-
+       
 
     }
 
