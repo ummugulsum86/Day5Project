86051060b5c7d82edbf06def340b120ba6b4a1ae
ummugulsum86 <72078416+ummugulsum86@users.noreply.github.com>
2020-10-19 13:09:15 +0200

    hello hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("sill really from github made some changes awesome hello");
+        System.out.println("sill really from github made some changes awesome hello another hello");
 
 
 
