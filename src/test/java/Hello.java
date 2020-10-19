6e411f66d45ef19d094a656f3e8b78a6d44b230e
Ummgulsum Koc <umgk86@gmail.com>
2020-10-19 13:10:33 +0200

    added comment on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,10 +1,13 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("sill really from github made some changes awesome hello");
 
-        //add one more line on your local and commit
 
+
+        System.out.println("sill really awesome hello");//added comment
+=========
+        System.out.println("silly really from github made some changes awesome hello");
+ >>>>>>>>>origin/master
 
 
     }
